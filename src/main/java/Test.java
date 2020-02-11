6f1c4117cc01122538577938e0ccc897a01dd564
chenqiang <cq291087689@163.com>
2020-02-11 17:52:33 +0800

    v2.0

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello flink");
+        System.out.println("hello girl");
     }
 }
